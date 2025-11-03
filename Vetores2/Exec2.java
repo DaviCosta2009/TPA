1026b032378ac69c5a9365f27b9212255cb4a799
DaviCosta2009 <DaviCostaSilva3008@gmail.com>
2025-11-03 11:58:11 -0300

    Create Exec2.java

---
+++ Vetores2/Exec2.java
@@ -0,0 +1,29 @@
+package vetores2;
+
+import java.util.Scanner;
+
+public class Exec2 {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        int[] A = new int[5];
+
+     
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Digite o " + (i + 1) + "º número inteiro: ");
+            A[i] = leia.nextInt();
+        }
+
+       
+        System.out.println("\n--- Tabuadas ---\n");
+        for (int elemento : A) {
+            System.out.println("Tabuada do " + elemento + ":");
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(elemento + " x " + i + " = " + (elemento * i));
+            }
+            
+            System.out.println();
+        }
+        
+        leia.close();
+    }
+}
