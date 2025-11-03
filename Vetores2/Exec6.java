7dc0b632444b0d522c96a01d5adc67181156692f
DaviCosta2009 <DaviCostaSilva3008@gmail.com>
2025-11-03 11:47:46 -0300

    Create Exec6.java

---
+++ Vetores2/Exec6.java
@@ -0,0 +1,33 @@
+package vetores2;
+
+public class Exec6 { 
+
+    public static void main(String[] args) {
+
+    	
+    	final int TAM = 11; 
+    	
+    	int A[]; 
+
+    	A = new int[TAM];
+
+
+        int potenciaA = 1; 
+        int i;
+        
+        i = 0;
+        while (i < TAM) {
+            
+            A[i] = potenciaA; 
+            
+            
+            System.out.println("A[" + i + "] = 2^" + i + " = " + A[i]);
+            
+            
+            
+            potenciaA = potenciaA * 2; 
+            i = i + 1;
+        }
+        
+    }
+}
