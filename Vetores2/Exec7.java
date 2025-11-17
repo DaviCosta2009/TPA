bc906c2ba7a4e68e4a5cceb24259c03e0a04933b
DaviCosta2009 <DaviCostaSilva3008@gmail.com>
2025-11-16 21:23:21 -0300

    Create Exec7.java

---
+++ Vetores2/Exec7.java
@@ -0,0 +1,52 @@
+package vetores2;
+
+import java.util.Scanner;
+
+public class Exec7 {
+
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        int TAM = 15;
+        
+        int[] A = new int[TAM];
+        int[] B = new int[TAM]; 
+        
+        int i;
+        int j;
+        
+        int numero;
+        int fatorial;
+
+        for (i = 0; i < TAM; i++) {
+            System.out.print("Digite o " + (i + 1) + "° numero inteiro (Recomendado 0 a 12): ");
+            A[i] = leia.nextInt();
+        }
+
+        
+        System.out.println("\n--- Fatoriais ---\n");
+        for (i = 0; i < TAM; i++) {
+            numero = A[i];
+            
+            
+            fatorial = 1;
+            
+            if (numero < 0) {
+                fatorial = 0; 
+            } else {
+               
+                j = 2;
+                while (j <= numero) {
+                    fatorial = fatorial * j;
+                    j = j + 1;
+                }
+            }
+            
+            B[i] = fatorial;
+            System.out.println("Fatorial de " + A[i] + " é: " + B[i]);
+            
+
+        }
+        
+        leia.close();
+    }
+}
